8acb4b3a923ef47ffceb67d08248f14a065115b2
HaesunByun <ladybhs@snu.ac.kr>
2023-04-26 21:57:55 +0900

    Update documentation

---
+++ jupyter_execute/Lecture-1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Lecture-
+# 
+# 안녕하세요.
+# 
+# 
+# 
+
+# In[1]:
+
+
+print('Hello World')
+
+
+# ## test
+
+# In[2]:
+
+
+a=10
+print(a)
+
+
+# There is a lot more that you can do with outputs (such as including interactive outputs)
+# with your book. For more information about this, see [the Jupyter Book documentation](https://jupyterbook.org)
